8560d70453bba6f6c059a654df8af38be73591a1
xiaomeng9597 <519561910@qq.com>
2026-07-22 07:13:32 +0800

    更新

--- diy-part2-6.x.sh
+++ diy-part2-6.x.sh
@@ -24,6 +24,7 @@ CONFIG_NET_DSA_YT921X=m
 CONFIG_NET_DSA_TAG_YT921X=m
 CONFIG_NET_DSA_YT921X_DEBUG=y
 CONFIG_NET_DSA_YT921X_CR881X=n" >> target/linux/rockchip/armv8/config-6.6
+ls target/linux/rockchip/armv8/config-6.6
 
 
 # 集成CPU性能跑分脚本
