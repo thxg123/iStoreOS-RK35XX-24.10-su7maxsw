dae93ed17bcc41b78f7cb5a29b1de450f4858f14
xiaomeng9597 <519561910@qq.com>
2026-07-22 15:57:47 +0800

    更新2

--- diy-part2-6.x.sh
+++ diy-part2-6.x.sh
@@ -66,3 +66,6 @@ ls target/linux/generic/files
 
 
 cp -f $GITHUB_WORKSPACE/configfiles/driver/999-net-dsa-add-yt921x-header-defs.patch target/linux/rockchip/patches-6.6/999-net-dsa-add-yt921x-header-defs.patch
+
+
+cp -f $GITHUB_WORKSPACE/configfiles/netdevices.mk package/kernel/linux/modules/netdevices.mk
