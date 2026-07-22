1e9cc328826b4e0e0bed1c88abf7698d86553f4b
xiaomeng9597 <519561910@qq.com>
2026-07-22 21:06:29 +0800

    更新

--- diy-part2-6.x.sh
+++ diy-part2-6.x.sh
@@ -20,8 +20,8 @@ cp -a $GITHUB_WORKSPACE/configfiles/etc/* package/base-files/files/etc/
 # 追加自定义内核配置项
 echo "CONFIG_PSI=y
 CONFIG_KPROBES=y
-CONFIG_NET_DSA_YT921X=m
-CONFIG_NET_DSA_TAG_YT921X=m
+CONFIG_NET_DSA_YT921X=y
+CONFIG_NET_DSA_TAG_YT921X=y
 CONFIG_NET_DSA_YT921X_DEBUG=y
 CONFIG_NET_DSA_YT921X_CR881X=y" >> target/linux/rockchip/armv8/config-6.6
 cat target/linux/rockchip/armv8/config-6.6
@@ -68,4 +68,4 @@ ls target/linux/generic/files
 cp -f $GITHUB_WORKSPACE/configfiles/driver/999-net-dsa-add-yt921x-header-defs.patch target/linux/rockchip/patches-6.6/999-net-dsa-add-yt921x-header-defs.patch
 
 
-cp -f $GITHUB_WORKSPACE/configfiles/netdevices.mk package/kernel/linux/modules/netdevices.mk
+# cp -f $GITHUB_WORKSPACE/configfiles/netdevices.mk package/kernel/linux/modules/netdevices.mk
