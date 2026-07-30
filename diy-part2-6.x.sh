71267c3ae4a6b2a34c905caeeee44108789f5dec
xiaomeng9597 <519561910@qq.com>
2026-07-30 08:17:02 +0800

    更新26

--- diy-part2-6.x.sh
+++ diy-part2-6.x.sh
@@ -73,6 +73,6 @@ cp -f $GITHUB_WORKSPACE/configfiles/driver/999-net-dsa-add-yt921x-header-defs.pa
 # cp -f $GITHUB_WORKSPACE/configfiles/netdevices.mk package/kernel/linux/modules/netdevices.mk
 
 
-# 首次重载网络配置文件
-# cp -f $GITHUB_WORKSPACE/configfiles/dsa_install package/base-files/files/etc/init.d/dsa_install
-# chmod 755 package/base-files/files/etc/init.d/dsa_install
+# 首次执行初始化脚本文件
+cp -f $GITHUB_WORKSPACE/configfiles/g98_init package/base-files/files/etc/init.d/g98_init
+chmod 755 package/base-files/files/etc/init.d/g98_init
