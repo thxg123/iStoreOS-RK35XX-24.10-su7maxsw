e5e0e47ef7da30a2be37242c79efcf166632cda8
xiaomeng9597 <519561910@qq.com>
2026-08-02 21:08:44 +0800

    更新36

--- diy-part2-6.x.sh
+++ diy-part2-6.x.sh
@@ -68,8 +68,8 @@ ls target/linux/generic/files
 
 
 cp -f $GITHUB_WORKSPACE/configfiles/driver/999-01-net-dsa-add-yt921x-header-defs.patch target/linux/rockchip/patches-6.6/999-01-net-dsa-add-yt921x-header-defs.patch
-cp -f $GITHUB_WORKSPACE/configfiles/driver/999-02-fix-pagepool-destroy.patch target/linux/rockchip/patches-6.6/999-02-fix-pagepool-destroy.patch
-cp -f $GITHUB_WORKSPACE/configfiles/driver/999-03-fix-tx-timeout.patch target/linux/rockchip/patches-6.6/999-03-fix-tx-timeout.patch
+# cp -f $GITHUB_WORKSPACE/configfiles/driver/999-02-fix-pagepool-destroy.patch target/linux/rockchip/patches-6.6/999-02-fix-pagepool-destroy.patch
+# cp -f $GITHUB_WORKSPACE/configfiles/driver/999-03-fix-tx-timeout.patch target/linux/rockchip/patches-6.6/999-03-fix-tx-timeout.patch
 
 # cp -f $GITHUB_WORKSPACE/configfiles/netdevices.mk package/kernel/linux/modules/netdevices.mk
 
@@ -77,3 +77,7 @@ cp -f $GITHUB_WORKSPACE/configfiles/driver/999-03-fix-tx-timeout.patch target/li
 # 启动时执行初始化脚本
 # cp -f $GITHUB_WORKSPACE/configfiles/g98_init package/base-files/files/etc/init.d/g98_init
 # chmod 755 package/base-files/files/etc/init.d/g98_init
+
+cp -f $GITHUB_WORKSPACE/configfiles/stmmac_main.c.txt stmmac_main.c.txt
+cp -f $GITHUB_WORKSPACE/configfiles/Makefile-dsa.txt target/linux/rockchip/Makefile
+ls
