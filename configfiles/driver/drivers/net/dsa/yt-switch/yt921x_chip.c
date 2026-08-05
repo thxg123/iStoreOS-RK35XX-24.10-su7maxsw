beae6eab801f82f0eee24f16bb608f9ff9cb617a
xiaomeng9597 <519561910@qq.com>
2026-08-05 10:16:29 +0800

    更新67

--- configfiles/driver/drivers/net/dsa/yt-switch/yt921x_chip.c
+++ configfiles/driver/drivers/net/dsa/yt-switch/yt921x_chip.c
@@ -1052,9 +1052,9 @@ static int yt921x_chip_setup_dsa(struct yt921x_priv *priv)
 	if (of_machine_is_compatible("xiaomi,cr881x") &&
 	    priv->secondary_cpu_port >= 0 &&
 	    priv->dt_secondary_conduit_user_mask_valid) {
-		u32 allowed_mask = priv->dt_secondary_conduit_user_mask &
+		u32 allowed_mask_cr = priv->dt_secondary_conduit_user_mask &
 			       yt921x_non_cpu_port_mask(priv);
-		u32 blocked_mask_cr = yt921x_non_cpu_port_mask(priv) & ~allowed_mask;
+		u32 blocked_mask_cr = yt921x_non_cpu_port_mask(priv) & ~allowed_mask_cr;
 
 		res = yt921x_port_isolation_set(priv, priv->secondary_cpu_port,
 						blocked_mask_cr);
