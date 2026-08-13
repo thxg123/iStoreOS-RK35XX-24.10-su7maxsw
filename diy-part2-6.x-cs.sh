d673ac92c3e159dda2a7c28e0d1697eddbdf5312
xiaomeng9597 <519561910@qq.com>
2026-08-13 15:20:03 +0800

    更新127

--- diy-part2-6.x-cs.sh
+++ diy-part2-6.x-cs.sh
@@ -78,9 +78,6 @@ sed -i '/for e in \$val; do json_add_string "" "\$e"; done/c\
 # cp -f $GITHUB_WORKSPACE/configfiles/netdevices.mk package/kernel/linux/modules/netdevices.mk
 
 
-cp -f $GITHUB_WORKSPACE/configfiles/userpatches/850-regulator-fixed-check-supply-before-request-gpio.patch target/linux/generic/hack-6.6/850-regulator-fixed-check-supply-before-request-gpio.patch
-
-
 # 启动时执行初始化脚本
 # cp -f $GITHUB_WORKSPACE/configfiles/g98_init package/base-files/files/etc/init.d/g98_init
 # chmod 755 package/base-files/files/etc/init.d/g98_init
